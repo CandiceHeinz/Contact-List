db6d4c2ddc1174056959d08beb5630de3a130f97
CandiceHeinz <CandiceHeinz@comcast.net>
2023-07-24 11:42:59 -0400

    confused but this is what i have so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react"; 
+import {useEffect} from 'react'
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+useEffect(() => {
+  async function fetchContacts() {
+    try {
+      const response = await fetch(
+        "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+      );
+      const result = await response.json();
+      setContacts(result);
+      } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchContacts()
+}, []);
+
+
 
